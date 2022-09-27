ba7c5193b955a38dd713da6ddf764d9cbd73ae8e
officialksolomon <solomonuche42@gmail.com>
2022-09-27 15:28:49 +0100

    changes...

--- src/components/Home/HomeAvatar.jsx
+++ src/components/Home/HomeAvatar.jsx
@@ -14,7 +14,7 @@ function HomeAvatar ({ change }) {
     <>
       <Col xs={12} md={change ? 6 : 8} className={`d-flex  ${change ? 'justify-content-end ' : 'justify-content-center'} align-items-center p-4`}>
         <div className='position-relative '>
-          <img onMouseEnter={(event) => changeImage(event)} onMouseLeave={(event) => changeImage(event)} src={src} className="avatar rounded-circle animate__animated animate__zoomIn" width={change ? 350 : 200} height={change ? 350 : 200} alt="Avatar" />
+          <img onMouseEnter={(event) => changeImage(event)} onMouseLeave={(event) => changeImage(event)} src={src} className="avatar rounded-circle animate__animated animate__zoomIn" width={change ? 350 : 250} height={change ? 350 : 250} alt="Avatar" />
           <span onMouseEnter={(event) => changeImage(event)} onMouseLeave={(event) => changeImage(event)} className=' avatar-span  position-absolute start-0 bottom-0 m-3 text-white text-center bg-black p-2 rounded opacity-75 '> Want to see Solomon? <br /> <span class="d-none d-sm-inline fw-bolder">Hover!</span> <span class=" d-sm-none fw-bolder">Click!</span></span>
         </div>
       </Col>
