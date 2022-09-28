3d11ad8292c3467d9908b79b10bc76978fea42f2
officialksolomon <solomonuche42@gmail.com>
2022-09-28 08:49:58 +0100

    changes....

--- src/components/Works/WorkList.jsx
+++ src/components/Works/WorkList.jsx
@@ -8,7 +8,7 @@ function WorkItem () {
     <>
       <Col xs={12} sm={6} md={4} className=''>
         <div className='rounded d-flex justify-content-center align-items-center border border-light border-opacity-25 position-relative'>
-          <img src={work3} alt="work" className='w-100' />
+          <img src={work3} alt="work" className='' />
           <span className='postion-absolute start-0 end-0'>InfoR</span>
         </div>
       </Col>
