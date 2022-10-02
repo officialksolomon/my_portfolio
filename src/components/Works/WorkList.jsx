082adf12a541cfb9d2b07e26163fb71a64a7242a
officialksolomon <solomonuche42@gmail.com>
2022-10-02 20:39:56 +0100

    changes...

--- src/components/Works/WorkList.jsx
+++ src/components/Works/WorkList.jsx
@@ -3,6 +3,7 @@ import WorkItem from './WorkItem'
 import work3 from '../../assets/images/my-work/work3.png'
 import work7 from '../../assets/images/my-work/work7.png'
 import work8 from '../../assets/images/my-work/work8.png'
+import work6 from '../../assets/images/my-work/work6.png'
 
 
 
@@ -19,8 +20,8 @@ function WorkList () {
       <WorkItem name={'Lazer'} img={work8}
         description={"Lazer P2P marketplace for trading their digital asset such as  Crypto assets with vendors."}
         sourceLink={'https://github.com/ulims/lazer'} liveLink={'https://lazerulims.herokuapp.com/'} />
-    </>
-      <WorkItem name={'JSBC'} img={work8}
+
+      <WorkItem name={'JSBC'} img={work6}
         description={"JSBC is a Forex Company Website know for helping investors earn money from the forex market through account management coaching etc...."}
         sourceLink={'https://github.com/officialksolomon/jsbc'} liveLink={'https://www.jsbc.com.ng/'} />
     </>
