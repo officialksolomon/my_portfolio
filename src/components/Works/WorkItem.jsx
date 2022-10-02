8e6daef205e1d61deed1d4cdb278a421173a483a
officialksolomon <solomonuche42@gmail.com>
2022-10-02 20:15:53 +0100

    changes...

--- src/components/Works/WorkItem.jsx
+++ src/components/Works/WorkItem.jsx
@@ -12,7 +12,7 @@ function WorkItem ({ img, name, description, sourceLink, liveLink }) {
   }
   return (
     <>
-      <Col xs={12} sm={6} md={4} className='overflow-scroll '>
+      <Col xs={12} sm={6} md={4} className=''>
         <div onMouseEnter={(event) => handleOpen(event)} onMouseLeave={(event) => handleOpen(event)} className='rounded d-flex flex-column justify-content-center align-items-center position-relative p-2 rounded p-0 h-100 bg-white'>
           <span className='position-absolute start-0 top-50 opacity-75 p-2 px-4 text-white bg-black w-auto rounded-end'>{name}</span>
           <span className='position-absolute start-0  top-50  p-2 px-4 text-white w-auto rounded-end shadow'>{name}</span>
